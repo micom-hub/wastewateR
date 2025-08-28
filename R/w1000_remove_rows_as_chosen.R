3a1d6145fc2817659cda3e4d6c74e90e60f03965
Gilbert <juliegil@UMICH.EDU>
2025-08-28 14:29:26 -0400

    new rule for increased sensitivity targets

--- R/w1000_remove_rows_as_chosen.R
+++ R/w1000_remove_rows_as_chosen.R
@@ -1,80 +1,87 @@
 
 #' Removing QAQC Rule Breakers
-#' 
-#' This function takes in a data frame of laboratory data that has been through 
-#' most if not all of the prior QAQC checks. It also takes as input, a vector 
-#' of numbers that correspond to the rules that, if there were only "mild" rule breaks 
-#' (aka that did not previously trigger a STOP), you'd like to remove those rule breaking rows. 
+#'
+#' This function takes in a data frame of laboratory data that has been through
+#' most if not all of the prior QAQC checks. It also takes as input, a vector
+#' of numbers that correspond to the rules that, if there were only "mild" rule breaks
+#' (aka that did not previously trigger a STOP), you'd like to remove those rule breaking rows.
 #' The default value for this vector is 'c(2, 3, 5, 6)'.
-#' 
-#' This function can currently accommodate this process for rules: 
-#' 
-#' - 2.0 Accepted Droplet Count Check
-#' - 3.0 NTC Control Check
-#' - 5.0 Control Warning - Positives Droplets 
-#' - 6.0 Extraction & Negative Control Check
-#' - 7.0 Sample Positive Droplet Sum Check
-#' - 8.0 Recovery Control Check
-#' - 9.0 Target Positives Droplet High Limit
-#' 
-#' For all of these rules, this function removes any rows with a value of 1 in 
-#' the relevant mark column. In addition, for 2.0 Accepted Droplet Count Check, 
-#' if any sample set had more than one well that broke the rule within the set, 
-#' the entire sample set is removed.  
-#' 
+#'
+#' This function can currently accommodate this process for rules:
+#'
+#' - 2.0 Accepted Droplet Count Check (2)
+#' - 3.0 NTC Control Check (3)
+#' - 5.0 Control Warning - Positives Droplets (5)
+#' - 6.0 Extraction & Negative Control Check (6)
+#' - 7.0 Sample Positive Droplet Sum Check (7)
+#' - 7.5 Sample Negative Droplet Sum Check (7.5)
+#' - 8.0 Recovery Control Check (8)
+#' - 9.0 Target Positives Droplet High Limit (9)
+#'
+#' For all of these rules, this function removes any rows with a value of 1 in
+#' the relevant mark column. In addition, for 2.0 Accepted Droplet Count Check,
+#' if any sample set had more than one well that broke the rule within the set,
+#' the entire sample set is removed.
+#'
 #' @param data_file_in A dataframe of laboratory data
 #' @param rules_out_v A vector of numbers that correspond to the rules that, if there were only "mild" rule breaks (aka that did not previously trigger a STOP), you'd like to remove those rule breaking rows. The default value for this vector is 'c(2, 3, 5, 6)'.
 #' @return A dataframe just like the input data frame, with any indicated rule breaker rows removed
 #' @export
 
 w1000_remove_rows_as_chosen <- function(data_file_in, rules_out_v = c(2, 3, 5, 6)){
-  
+
   if (2 %in% rules_out_v){
-    
+
     data_file_in <- filter(data_file_in, accepted_droplet_limit2 != 1)
     data_file_in <- filter(data_file_in, accepted_droplet_count2 < 2)
-    
+
   }
-  
+
   if (3 %in% rules_out_v){
-    
+
     data_file_in <- filter(data_file_in, ntc_control_check3 != 1)
-    
+
   }
-  
+
   if (5 %in% rules_out_v){
-    
+
     data_file_in <- filter(data_file_in, control_pos_drop_soft != 1 | is.na(control_pos_drop_soft))
-    
+
   }
-  
+
   if (6 %in% rules_out_v){
-    
+
     data_file_in <- filter(data_file_in, ext_neg_control_check6 != 1 | is.na(ext_neg_control_check6 ))
-    
+
   }
-  
+
   if (7 %in% rules_out_v){
-    
+
     data_file_in <- filter(data_file_in, sample_wells_positives != 1 | is.na(sample_wells_positives))
-    
+
   }
-  
+
+  if (7.5 %in% rules_out_v){
+
+    data_file_in <- filter(data_file_in, sample_wells_negatives != 1 | is.na(sample_wells_negatives))
+
+  }
+
   if (8 %in% rules_out_v){
-    
+
     data_file_in <- filter(data_file_in, bcov_flag != 1)
-    
-  } 
-  
-  
-  if (9 %in% rules_out_v){
-    
-    data_file_in <- filter(data_file_in, sample_pos_limit_flag != 1)
-    
+
   }
-  
+
+
+  if (9 %in% rules_out_v){
+
+    data_file_in <- filter(data_file_in, sample_pos_limit_flag != 1)
+
+  }
+
   return(data_file_in)
-  
+
 }
 
 
