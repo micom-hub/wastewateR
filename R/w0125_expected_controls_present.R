5775b955715c5f848e23292c90fe7d30fb7d64a4
Gilbert <juliegil@UMICH.EDU>
2025-12-22 14:50:45 -0500

    changing for pipeline development output as list now

--- R/w0125_expected_controls_present.R
+++ R/w0125_expected_controls_present.R
@@ -16,15 +16,22 @@
 #' Please note: This check will not alert if there is a control that is NOT
 #' mentioned.
 #'
+#' if stop_choice is "yes" then:
 #' STOP ALERT: If there are any instances of inconsistencies between the actual
 #' number of control wells identified and the expected number as indicated by
 #' the two input vectors, the offending control type and the well counts will be
 #' printed to the console. The code will STOP RUNNING if this occurs.
 #'
+#' if stop_choice is "no" then:
+#' The error information will print to the console and the return list will have the
+#' output data frame as the first element in the list and a stop indicator of 1 as the
+#' second element in the list.
+#'
 #' @param df_in A dataframe of laboratory data, must contain a column called Sample
 #' @param exp_cntrl_v A vector of character strings that indicate control wells. Default vector is "NEG", "POS", "NTC", "BCOV", "EXT"
 #' @param expected_count A numeric vector of the expected number of wells for each control type. Default vector is 3, 3, 3, 3, 3
-#' @return A dataframe identical to df_in
+#' @param stop_choice A character string of "yes" or "no" to indicate whether this should be a hard stop function or not
+#' @return A list with the first element being a dataframe identical to df_in, and the second element being either 0 (for no failure stop) or 1 (for failure stop)
 #' @export
 
 w0125_expected_controls_present <- function(df_in, exp_cntrl_v = c("NEG", "POS", "NTC", "BCOV", "EXT"), expected_count = c(3, 3, 3, 3, 3)){
@@ -84,7 +91,16 @@ w0125_expected_controls_present <- function(df_in, exp_cntrl_v = c("NEG", "POS",
 
       }
 
-      stop()
+      if (stop_choice == "yes"){
+          stop()
+      } else if (stop_choice == "no"){
+          stopper_ind <- 1
+          message("Stop error encountered - #1.25")
+      }
+
+    } else {
+
+      stopper_ind <- 0
 
     }
 
@@ -92,5 +108,6 @@ w0125_expected_controls_present <- function(df_in, exp_cntrl_v = c("NEG", "POS",
     message("") # just for visual clarity
     message("Through Check #1.25") # notify the check is over
 
-    return(df_in) # return our input
+    return(list(df_in, stopper_ind)) # return our input
+
 }
