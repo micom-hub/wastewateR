1ebfa4e8fc39daed88405f6eab378f4c11fed045
Gilbert <juliegil@UMICH.EDU>
2025-12-23 16:17:50 -0500

    reference change in functions

--- R/qaqc_processing_norog1g2.R
+++ R/qaqc_processing_norog1g2.R
@@ -64,13 +64,13 @@ qaqc_processing_norog1g2 <- function(file_in, lab_site_ids,
   error_line <- c(error_line, "w0650")
   error_val <- c(error_val, w0650_cumulative_count_check(file_in[1][[1]], 3, "yes"))
 
-  file_in <- w0700_pos_droplet_sum(file_in[1][[1]], control_strings)
+  file_in <- w0700_pos_droplet_sum(file_in[1][[1]], controls_to_drop = con_rows)
 
-  file_in <- w0750_neg_droplet_samples(file_in, controls_to_drop = control_strings)
+  file_in <- w0750_neg_droplet_samples(file_in, controls_to_drop = con_rows)
   error_line <- c(error_line, "w0750")
   error_val <- c(error_val, file_in[2][[1]])
 
-  file_in <- w0900_positives_comparison_rule(file_in[1][[1]], c("POS"), c("NVG1", "NVG2"), c("POS", "NEG", "EXT", "NTC"), 3)
+  file_in <- w0900_positives_comparison_rule(file_in[1][[1]], c("POS"), c("NVG1", "NVG2"), control_strings, 3)
 
   file_in <- w1000_remove_rows_as_chosen(file_in, rules)
 
