5e69ee966ed95eac80a7bbd926873e0e0c88a361
Gilbert <juliegil@UMICH.EDU>
2026-02-24 09:32:38 -0500

    cleaned up documentation

--- R/w0110_sample_name_edits.R
+++ R/w0110_sample_name_edits.R
@@ -6,7 +6,7 @@
 #' 2.  Changes all letter characters in `Sample` and `Target` to capitalized / uppercase letters.
 #'
 #' @param df_in Data frame of laboratory data, needs to have a column called "Sample" and a column called "Target"
-#' @return A data frame with (possibly) edited sample names
+#' @return A data frame with (possibly) edited sample and target column contents
 #' @export
 
 w0110_sample_name_edits <- function(df_in){
@@ -14,17 +14,18 @@ w0110_sample_name_edits <- function(df_in){
 ################################################################################
 # Check #1.1: Edit Sample Names as necessary
 
-    message("CHECK #1.1: Sample Name Edits for Consistency")
+    message("CHECK #1.1: Sample & Target Column Name Edits for Consistency")
 
-    # remove interior spaces, change BCOV to BCoV, make sure all other sample names
-    # have letters that are all capitals
+    # sample column edited first
+
+    # remove interior spaces and leading/lagging spaces
     df_in <- df_in %>% mutate(Sample = gsub(" ", "", trimws(Sample))) # removes interior spaces from all sample names
     # leading and lagging whitespace also removed
 
-    # maintains the BCoV change, and turns all other sample letters to uppercase, if they aren't already
+    # turns all other sample letters to uppercase, if they aren't already
     df_in <- df_in %>% mutate(Sample = toupper(Sample))
 
-    #message("Target Name Edits for Consistency")
+    #target column second, same rules
 
     df_in <- df_in %>% mutate(Target = gsub(" ", "", trimws(Target)))
 
